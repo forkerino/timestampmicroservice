e0915ef4d196002a3762e83111d7c2246c236518
Pieter <pieterstokkink@gmail.com>
2017-01-04 22:46:07 +0000

    added logic

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+'use strict';
+
 const express = require('express');
+const moment = require('moment');
 
 let app = express();
 
-app.get('/', function(req, res){
-     
+app.get('/:date', function(req, res){
+    let date = String(req.params.date);
+    if (moment(date, 'MMMM D, YYYY', true).isValid()){
+        res.end(JSON.stringify({ 'unix': moment(date).valueOf(), 'natural': date}));
+    } else if (moment(date, 'x', true).isValid()) { 
+        res.end(JSON.stringify({ 'unix': date, 'natural': moment(date, 'x').format('MMMM D, YYYY')}));
+    } else {
+        res.end(JSON.stringify({ 'unix': null, 'natural': null}));
+    }
 });
 
 app.listen(8080, function(){
     console.log("listening on port 8080");
-})
+});
